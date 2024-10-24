bc699c8798dc005e797a081486b825a7bce8924c
otaviovasc-inteli <otavio.vasconcelos@sou.inteli.edu.br>
2024-10-24 17:55:49 -0300

    feat - lp done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,5 @@
 class HomeController < ApplicationController
-  def index
+  def lp
   end
 
   def signup
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'home#index'
+  root 'home#lp'
   get '/signup', to: 'home#signup'
 
   # The OmniAuth callback route
